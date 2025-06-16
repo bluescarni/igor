db8873dd22449664e852708c935ef5efa8380d89
Francesco Biscani <bluescarni@gmail.com>
2025-06-16 14:25:12 +0200

    First stab at clang build.

---
+++ tools/gha_clang.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda create -y -p $deps_dir cmake ninja 'clang=14.*' 'clangxx=14.*'
+source activate $deps_dir
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# Clear the compilation flags set up by conda.
+unset CXXFLAGS
+unset CFLAGS
+
+# Configure.
+cmake ../ -G Ninja \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DIGOR_BUILD_TESTS=yes \
+    -DCMAKE_CXX_FLAGS="-fsanitize=address" \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og"
+
+# Build.
+ninja -v -j4
+
+# Run the tests.
+ctest -VV -j4
+
+set +e
+set +x
