bb397ed7c002c13419b7555ede767d0e0c389911
Francesco Biscani <bluescarni@gmail.com>
2025-06-16 14:32:01 +0200

    Try switching to osx.

--- tools/gha_clang.sh
+++ tools/gha_clang.sh
@@ -10,11 +10,11 @@ set -e
 sudo apt-get install wget
 
 # Install conda+deps.
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh -O miniconda.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda create -y -p $deps_dir cmake ninja 'clang=14.*' 'clangxx=14.*' 'libcxx=14.*'
+conda create -y -p $deps_dir cmake ninja 'clang=14.*' 'clangxx=14.*'
 source activate $deps_dir
 
 # Create the build dir and cd into it.
@@ -28,15 +28,12 @@ unset CFLAGS
 # Configure.
 cmake ../ -G Ninja \
     -DCMAKE_PREFIX_PATH=$deps_dir \
-    -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_C_COMPILER=clang \
     -DCMAKE_BUILD_TYPE=Debug \
     -DIGOR_BUILD_TESTS=yes \
-    -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
     -DCMAKE_CXX_FLAGS_DEBUG="-g -Og"
 
 # Build.
-ninja -v -j4
+ninja -v
 
 # Run the tests.
 ctest -VV -j4
