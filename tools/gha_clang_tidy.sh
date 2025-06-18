10bead5abc9872c41dde509282715a94795f6b03
Francesco Biscani <bluescarni@gmail.com>
2025-06-18 09:15:51 +0200

    Add clang tidy build.

---
+++ tools/gha_clang_tidy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda create -y -p $deps_dir ninja cmake c-compiler cxx-compiler clang clangxx clang-tools
+source activate $deps_dir
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# Clear the compilation flags set up by conda.
+unset CXXFLAGS
+unset CFLAGS
+
+# Configure.
+cmake ../ -G Ninja \
+    -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og" \
+    -DIGOR_BUILD_TESTS=yes \
+    -DCMAKE_CXX_CLANG_TIDY=`which clang-tidy` \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++
+
+# Build.
+ninja -v
+
+set +e
+set +x
